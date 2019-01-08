b33e0cd0a4826215f3b05791c9e0679c779575bd
brunoramos <brunotolomeotti@gmail.com>
2019-01-08 18:39:03 -0200

    Layout Implementation

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from './containers/layout/layout.component';
 
 const routes: Routes = [
-	{path: '', pathMatch: 'full', redirectTo: 'payment'},
-	{path: 'payment', loadChildren: '../payment/payment.module#PaymentModule'}
+	{path: '', pathMatch: 'full', redirectTo: 'layout'},
+	{
+		path: 'layout', component: LayoutComponent, children: [
+			{path: '', pathMatch: 'full', redirectTo: 'payment'},
+			{path: 'payment', loadChildren: '../payment/payment.module#PaymentModule'},
+		]
+	}
 ];
 
 @NgModule({
